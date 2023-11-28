fa21dd6e9dc5ebf19c777823745033a2b24775fe
tyandkuliah <maulanaseptian239@gmail.com>
2023-11-28 19:51:45 +0700

    baru

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -33,7 +33,7 @@ small">
                                             Me</label>
                                     </div>
                                 </div>
-                                <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                <a href="<?php echo base_url('dashboard') ?>" class="btn btn-primary btn-user btn-block">
                                     Login
                                 </a>
                                 <hr>
@@ -50,8 +50,7 @@ btn-user btn-block">
                                 <a class="small" href="forgot-password.html">Forgot Password?</a>
                             </div>
                             <div class="text-center">
-                                <a class="small" href="register.html">Create an
-                                    Account!</a>
+                                <a class="small" href="register.html">Create an Account!</a>
                             </div>
                         </div>
                     </div>
